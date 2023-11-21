d68e2b3e5c7b38e1222c6b6d58aeec9dc057b235
tool7831 <tool7831@gmail.com>
2023-11-21 22:12:30 +0900

    Feat: save and load session key using cookie

--- front/ga_react/src/App.js
+++ front/ga_react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Component} from 'react'
 import { BrowserRouter, Routes, Route }	from 'react-router-dom'
+import {CookiesProvider} from 'react-cookie'
 
 import Home	from './Home'
 import History	from './History'
 import Board from './pages/Board.js'
 import Tip from './pages/Tip.js'
-import Menubar from './components/Menubar.js'
+import Root from './pages/Root.js'
 import Compare	from './Compare'
 
 import './style.css'
@@ -15,17 +16,18 @@ class App extends Component {
 	render(){
 		return (
 			<div style={{height:"100%"}}>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" exact={true} element={<Menubar/>} />
-						<Route path="/home"  element={<Home/>} />
-						<Route path="/history"  element={<History/>} />
-						<Route path="/tips" exact={true} element={<Board/>}/>
-						<Route path='/tips/:idx' exact={true} element={<Tip/>}/>
-						<Route path="/compare"  element={<Compare/>} />
-					</Routes>
-				</BrowserRouter>
-				
+				<CookiesProvider>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" exact={true} element={<Root />} />
+							<Route path="/home" element={<Home />} />
+							<Route path="/history" element={<History />} />
+							<Route path="/tips" exact={true} element={<Board />} />
+							<Route path='/tips/:idx' exact={true} element={<Tip />} />
+							<Route path="/compare" element={<Compare />} />
+						</Routes>
+					</BrowserRouter>
+				</CookiesProvider>
 			</div>
 		);
 	}
---
+++ front/ga_react/src/pages/Root.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import {useCookies} from 'react-cookie'
+import axios from 'axios';
+import Menubar from '../components/Menubar.js'
+
+function Root(props) {
+    const [cookies, setCookie, removeCookie] = useCookies(['session_key']);
+    //componentDidMount =>
+	useEffect(() => {
+        const session_key = cookies.session_key
+        if (session_key === undefined){
+            console.log("session key doesn't exist");
+        }
+        else{
+            console.log(session_key);
+        }
+        const some_key = '123456';
+        setCookie('session_key', some_key);
+		console.log('component mounted!');
+	}, [])
+	return (
+        <div>
+            <Menubar/>
+        </div>
+	)
+}
+
+export default Root;
