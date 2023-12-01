977f821a76b4f3609cd35d9513416b0fafcea083
tool7831 <tool7831@gmail.com>
2023-12-01 23:14:04 +0900

    Fix: cors error fix

--- front/src/pages/Root.js
+++ front/src/pages/Root.js
@@ -8,29 +8,29 @@ function Root(props) {
     //componentDidMount =>
     useEffect(() => {
         const fetchData = async () => {
-            // const session_key = cookies.session_key
-            // const session_key = "12"
-            const headers = new Headers();
-            // headers.append('Authorization', session_key);
-            headers.append('Content-Type', 'application/json');
+            const session_key = cookies.session_key
+            const headers = {'Content-Type': 'application/json'}
+
+            console.log(session_key)
+            if (session_key !== undefined) 
+                headers['Authorization'] = session_key;
 
             // if (session_key === undefined) { console.log("session key doesn't exist"); }
             // else { console.log(session_key); }
+            // const response = await fetch('http://3.34.50.151:80', {
+            //     method: 'POST',
+            //     headers: headers
+            // });
+            // const result = await response.json();
+            // console.log(result);
 
-            const response = await fetch('http://3.34.50.151:80', {
-                method: 'POST',
-                headers: headers
-            });
-
-            const result = await response.json();
-            console.log(result);
-            // axios.post('/',null,{})
-            // .then(res => {
-            //     console.log('#result: ' + JSON.stringify(res.data));
-            //     setCookie('session_key', res.data.session_key);
-            // }).catch(error => {
-            //     alert('#save error ' + error)
-            // })
+            axios.post('/',null,{headers:headers})
+            .then(res => {
+                console.log('#result: ' + JSON.stringify(res.data));
+                setCookie('session_key', res.data.session_key);
+            }).catch(error => {
+                alert('#save error ' + error)
+            })
         }
         fetchData();
     }, [])
