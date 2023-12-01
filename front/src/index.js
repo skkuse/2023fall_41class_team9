1c344a4320a50427405893a306722ccf4dcdfb83
JH Lee <tool7831@gmail.com>
2023-12-01 17:51:45 +0900

    Bug: Cors error

--- front/src/index.js
+++ front/src/index.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 //axios.defaults.crossDomain = false;
 axios.defaults.withCredentials = true;
 //axios.defaults.withCredentials = false;
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://3.34.50.151'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- front/src/pages/Root.js
+++ front/src/pages/Root.js
@@ -6,24 +6,35 @@ import Menubar from '../components/Menubar.js'
 function Root(props) {
     const [cookies, setCookie, removeCookie] = useCookies(['session_key']);
     //componentDidMount =>
-	useEffect(() => {
-        const session_key = cookies.session_key
-        const headers = {'Authorization':session_key};
+    useEffect(() => {
+        const fetchData = async () => {
+            // const session_key = cookies.session_key
+            // const session_key = "12"
+            const headers = new Headers();
+            // headers.append('Authorization', session_key);
+            headers.append('Content-Type', 'application/json');
 
-        if (session_key === undefined){console.log("session key doesn't exist");}
-        else{console.log(session_key);}
-        axios.post('/', null,{headers:headers})
-        .then(res => {
-            console.log('#result: ' + JSON.stringify(res.data));
-            setCookie('session_key', res.data.session_key);
-        }).catch(error => {
-            alert('#save error ' + error)
-        })
+            // if (session_key === undefined) { console.log("session key doesn't exist"); }
+            // else { console.log(session_key); }
 
-        const some_key = Math.random() * 10000;
-        setCookie('session_key', some_key);
-		console.log('component mounted!');
-	}, [])
+            const response = await fetch('http://3.34.50.151:80', {
+                method: 'POST',
+                headers: headers
+            });
+
+            const result = await response.json();
+            console.log(result);
+            // axios.post('/',null,{})
+            // .then(res => {
+            //     console.log('#result: ' + JSON.stringify(res.data));
+            //     setCookie('session_key', res.data.session_key);
+            // }).catch(error => {
+            //     alert('#save error ' + error)
+            // })
+            console.log('component mounted!');
+        }
+        fetchData();
+    }, [])
 	return (
         <div>
             <Menubar/>
